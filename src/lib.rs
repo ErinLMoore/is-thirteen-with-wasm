77003a1ef5bf90ee68fec10eee94fdc25da5fde2
Erin Moore <erin.moore@joinroot.com>
2020-01-10 09:00:59 -0500

    adds is_thirteen function with tests; also readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn is_thirteen(possibly_thirteen: i32) -> bool{
+    possibly_thirteen == 13
+}
+
+
 #[cfg(test)]
 mod tests {
+    use is_thirteen;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn is_thirteen_test() {
+        assert_eq!(is_thirteen(13), true);
+    }
+    #[test]
+    fn is_not_thirteen_test() {
+        assert_eq!(is_thirteen(12), false);
     }
 }
